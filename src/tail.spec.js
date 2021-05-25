35ee41459daac9576043a7179714298846320067
Sri Devi Abhirami <srideviabhirami.v@thoughtworks.com>
2021-05-25 20:29:07 +0530

    Add tests for tail method

---
+++ src/tail.spec.js
@@ -0,0 +1,15 @@
+const tail = require('./tail');
+
+describe('Tail', () => {
+
+    it('should return the tail of the array [1,2,3] as 3', () => {
+        let arr = [1,2,3];
+        expect(tail(arr)).toEqual(3);
+    });
+
+    it('should return the tail of the array [] as undefined', () => {
+        let arr = [];
+        expect(tail(arr)).toEqual(undefined);
+    });
+
+})
